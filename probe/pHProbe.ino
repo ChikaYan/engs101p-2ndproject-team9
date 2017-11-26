50f3a46b1a6117e4492c088f2fc457097259b8a4
arcgt <30797041+arcgt@users.noreply.github.com>
2017-11-26 16:45:20 +0000

    Add comments

--- probe/pHProbe.ino
+++ probe/pHProbe.ino
@@ -1,12 +1,12 @@
-float pHX;
-float pHS = 7;
-//float offset;
-float Es = 0;
-float F = 9.6485309e4;
-float R = 8.314510;
-float T = 23.6 + 273.15;
-//int acidPump = 9;
-//int basePump = 10;
+int Vout = 6;  //analogue voltage input
+int acidPump = 9;  //digital voltage output to transistor gate for acid
+int basePump = 10;  //digital voltage output to transistor gate for base
+
+float pHs = 7.0;  //pH of standard solution
+float offset = 0.506;  //offset voltage
+float F = 9.6485309e4;  //Faraday's constant
+float R = 8.314510;  //universal gas constant
+float T = 23.6 + 273.15;  //temperature of solution (K)
 
 void setup() {
   Serial.begin(9600);
@@ -15,23 +15,23 @@ void setup() {
 }
 
 void loop() {
-  float sensorValue = analogRead(A4);
-//  float Ex = map(sensorValue, 0.0, 1023.0, 0.0, 3.0);
-  float Ex = sensorValue*(3/1023);
-  pHX = pHS + (((Es-Ex)*F)/(R*T*log(10)));
-  Serial.println(pHX);
-  Serial.println(Ex);
-  Serial.println(sensorValue);
+  float sensorValue = analogRead(Vout);  //sensor value for analogue voltage input
+//  ?doesn't work? float VpH = map(sensorValue, 0.0, 1023.0, 0.0, 3.0);
+  float Vin = sensorValue*(3.0/1023.0);  //voltage input
+  float VpH = Vin - offset;  //voltage produced by probe
+  float pHx = pHs + ((VpH*F)/(R*T*log(10)));  //pH of unknown solution
 
-//  if(pHX > 5.5) {
-//    digitalWrite(acidPump, HIGH);
+  Serial.println(sensorValue);  //test
+  Serial.println(pHx);  //test
+
+//  if(pHx > 5.5) {
+//    digitalWrite(acidPump, HIGH);  //supply acid
+//    analogWrite(basePump, LOW);  //do not supply base
 //  }
-//  else if(pHX < 4.5){
-//    digitalWrite(basePump, HIGH);
+//  else if(pHx < 4.5){
+//    digitalWrite(basePump, HIGH);  //supply base
+//    analogWrite(acidPump, LOW);  //do not supply acid
 //  }
 
-//  delay(2000);
-//  analogWrite(acidPump, LOW);
-//  analogWrite(basePump, LOW);
+  delay(2000);  //test
 }
-
