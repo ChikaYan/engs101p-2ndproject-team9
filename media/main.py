fbdeebdc1373fb792331eb2a663abb65022ce37b
Walter Wu <walterwuyan@gmail.com>
2017-12-06 10:29:53 +0000

    Remove useless methods

--- media/main.py
+++ media/main.py
@@ -9,23 +9,10 @@ import serial
 class Menu:
     def __init__(self, master):
         self.master = master
-        self.master.title("Bioreactor Controller Team 9")
-        self.master.geometry("200x300")
 
-        Label(text="Bioreactor Controller").grid(row=0, column=1, sticky=N)
-        Label(text="-- by ENGS101P 2nd Project Team 9").grid(row=1, column=1, sticky=N)
-
-        self.mix_button = ttk.Button(self.master, text="Mixing Control", command=self.open_mix)
-        self.mix_button.grid(row=2, column=1, sticky=N, padx=2, pady=6)
-        self.heat_button = ttk.Button(self.master, text="Heating Control", command=self.open_heat)
-        self.heat_button.grid(row=3, column=1, sticky=N, padx=2, pady=6)
-        self.ph_button = ttk.Button(self.master, text="pH Control", command=self.open_ph)
-        self.ph_button.grid(row=4, column=1, sticky=N, padx=2, pady=6)
-        self.exit_button = ttk.Button(self.master, text="Exit", command=self.master.quit)
-        self.exit_button.grid(row=5, column=1, sticky=N, padx=2, pady=20)
-
-        self.ser = serial.Serial("/dev/ttyACM0", 9600, timeout=0)
-        self.master.after(1000, self.read_serial())
+        self.ser = None
+        # self.ser = serial.Serial("/dev/ttyACM0", 9600, timeout=0)
+        # self.master.after(1000, self.read_serial())
 
         self.heat_control = HeatControl(self.master, "Heating Control", "Target Temperature: 30.0C",
                                         "Current Temperature: Unknown", True, self.ser)
@@ -36,14 +23,18 @@ class Menu:
         self.ph_control.hide()
         self.mix_control.hide()
 
-    def open_mix(self):
-        self.mix_control.show()
-
-    def open_heat(self):
-        self.heat_control.show()
-
-    def open_ph(self):
-        self.ph_control.show()
+        self.master.title("Bioreactor Controller Team 9")
+        self.master.geometry("200x300")
+        Label(text="Bioreactor Controller").grid(row=0, column=1, sticky=N)
+        Label(text="-- by ENGS101P 2nd Project Team 9").grid(row=1, column=1, sticky=N)
+        self.mix_button = ttk.Button(self.master, text="Mixing Control", command=self.mix_control.show)
+        self.mix_button.grid(row=2, column=1, sticky=N, padx=2, pady=6)
+        self.heat_button = ttk.Button(self.master, text="Heating Control", command=self.heat_control.show)
+        self.heat_button.grid(row=3, column=1, sticky=N, padx=2, pady=6)
+        self.ph_button = ttk.Button(self.master, text="pH Control", command=self.ph_control.show)
+        self.ph_button.grid(row=4, column=1, sticky=N, padx=2, pady=6)
+        self.exit_button = ttk.Button(self.master, text="Exit", command=self.master.quit)
+        self.exit_button.grid(row=5, column=1, sticky=N, padx=2, pady=20)
 
     def read_serial(self):  # To write Serial, just use ser.write() as bytes
         ser_input = self.ser.readline()  # reads bytes in
